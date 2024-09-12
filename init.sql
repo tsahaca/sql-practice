ede59d65b5da0c4fe233964a1737560df01d46dc
tsahaca <tanmay.x.saha@gmail.com>
2024-09-12 10:22:55 -0700

    added new files

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS users;
+
 -- Create the table
 CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY,
@@ -8,5 +10,29 @@ CREATE TABLE IF NOT EXISTS users (
 -- Copy data from the CSV file into the table
 COPY users(id, name, age) 
 FROM '/docker-entrypoint-initdb.d/data.csv' 
+DELIMITER ',' 
+CSV HEADER;
+
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL
+);
+
+COPY customers(customer_id, customer_name) 
+FROM '/docker-entrypoint-initdb.d/customers_sample.csv' 
+DELIMITER ',' 
+CSV HEADER;
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT REFERENCES customers(customer_id),
+    order_date DATE NOT NULL,
+    total_amount DECIMAL(10, 2) NOT NULL
+);
+
+COPY orders(order_id, customer_id, order_date, total_amount) 
+FROM '/docker-entrypoint-initdb.d/orders_sample.csv' 
 DELIMITER ',' 
 CSV HEADER;
